3b10a85ad59f0cbdfdbe68e223f4ee4a8de404ed
Ubuntu <ubuntu@ip-172-31-33-156.ap-northeast-1.compute.internal>
2020-10-11 13:18:14 +0000

    add delete

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -17,4 +17,7 @@
     </table>
     {{-- タスク編集ページへのリンク --}}
     {!! link_to_route('tasks.edit', 'このタスクを編集', ['task' => $task->id], ['class' => 'btn btn-light']) !!}
+    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
+    {!! Form::close() !!}
 @endsection
